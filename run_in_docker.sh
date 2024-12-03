38731bf6a8196a5d88db7858ace82d6df1674489
itachi <shimin321@outlook.com>
2024-12-03 09:43:05 +0800

    Fixed docker image version

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,4 +1,4 @@
 MAX_MEM=180g
 MAX_JOBS=32
 
-docker run --rm -it -m $MAX_MEM -v $(pwd):/root ghcr.io/luojiazhishu/tamarin-docker/cli:latest bash -c "apt update && apt install -y make && make all -j ${MAX_JOBS}"
+docker run --rm -it -m $MAX_MEM -v $(pwd):/root ghcr.io/luojiazhishu/tamarin-docker/cli:v1.8.0 bash -c "apt update && apt install -y make && make all -j ${MAX_JOBS}"
